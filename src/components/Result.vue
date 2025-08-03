6a620472779e38a18ca76356a44b26b7855c527d
dimuthx353 <dimuthx353@gmail.com>
2025-08-03 21:24:02 +0530

    Add Questions and Result components; implement quiz logic and structure

---
+++ src/components/Result.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="result">
+    <div class="title">You got sample result 1!</div>
+    <div class="desc">Enter a short description here about the result.</div>
+  </div>
+</template>
+
+<script></script>
+
+<style lang="scss" scoped></style>
